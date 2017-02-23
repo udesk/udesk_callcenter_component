d16c780a5c83a93b009b6e664feb4c4ec544134f
jiawei <f37fanjiawei@126.com>
2017-02-23 17:05:25 +0800

    拆分css和js，压缩代码

--- README.md
+++ README.md
@@ -1,36 +1,53 @@
-使用方法
------
+dist目录下是已经编译压缩以后的代码，可以直接放到你的项目里使用。
+如果修改了我的代码，则需要先编译，编译方法在[这里](#user-content-如何编译)。
 
-#### 1、html中引入dist目录下的callcenter-component.js
+直接使用
+----
+
+#### 1、将dist目录下的内容引入你自己的项目，fonts目录拷贝到你的项目静态资源目录下。
 
 ```html
-<script src='./dist/callcenter-component.js'></script>
+<!--  引入css-->
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+<!--  引入js-->
+<script src='./callcenter-component.js'></script>
 ```
 
 #### 2、初始化组件
 
-```javascript
+```html
+<script>
 new UdeskCallcenterComponent({
     container: document.body,    //组件的容器
-    token: 'xxxxxxxx',  //通过登录接口获取的客服token，接口文档在doc/登录.md
-    subDomain: 'udesk'  //在udesk注册的二级域名
+    token: 'xxxxxxxx',           //通过登录接口获取的客服token，接口文档在doc/登录.md
+    subDomain: 'udesk'           //在udesk注册的二级域名
 });
+</script>
 ```
 
 如何编译
------
+----
+
+#### 1、安装nodejs
+
+[下载后安装](https://nodejs.org/zh-cn/download/)
+
+#### 2、安装webpack
 
 项目用webpack打包，需要先安装webpack
 ```
 npm install -g webpack
 ```
 
-安装依赖
+#### 3、下载依赖
+
+进入项目目录，运行下面的命令
 ```
 npm install
 ```
 
-编译
+#### 4、编译，打包，压缩
+
 ```
 webpack
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,43 @@
-module.exports = {
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = [{
     entry: './js/main.js',
     output: {
         path: './dist/',
         filename: 'callcenter-component.js'
     },
     module: {
-        loaders: [
-            { test: /\.css$/, exclude: /node_modules/, loader: 'style!css' },
+        rules: [
             {
-                test: /\.scss$/, exclude: /node_modules/, loaders: ['style-loader', 'css-loader?minimize', 'sass-loader'
-            ]
+                test: /\.css$/, exclude: /node_modules/, loader: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                loaders: ExtractTextPlugin.extract({
+                    fallback: 'style-loader', use: ['css-loader?minimize', 'sass-loader']
+                })
             },
             { test: /\.js|jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff&publicPath=dist/" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?publicPath=dist/" }
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?name=fonts/[name].[ext]&limit=10000&mimetype=application/font-woff&publicPath=/"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader?name=fonts/[name].[ext]&publicPath=/"
+            }
         ]
-    }
-};
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new ExtractTextPlugin("css/style.css"),
+        new UglifyJSPlugin()
+    ]
+}
+];
