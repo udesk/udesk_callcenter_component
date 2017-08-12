63ebc937899fc948016896be1515ad8cc05c6169
jiawei <f37fanjiawei@126.com>
2017-08-12 16:10:13 +0800

    新增销毁组件的方法

--- js/component/Alert.js
+++ js/component/Alert.js
@@ -1,37 +1,46 @@
 var doc = document;
-var timeoutId;
-var Alert = function(text, type) {
-    var id = 'udesk-callcenter-component-alert';
-    var element = doc.getElementById(id);
-    var body = doc.body;
 
-    if (!element) {
-        element = doc.createElement('div');
-        element.id = id;
+class Alert {
+    createElement() {
+        var id = 'udesk-callcenter-component-alert';
+        var element = this.element = doc.getElementById(id);
+        var body = doc.body;
 
-        body.append(element);
+        if (!element) {
+            element = this.element = doc.createElement('div');
+            element.id = id;
+
+            body.append(element);
+        }
     }
 
-    element.innerText = text;
-
-    if (type === 'success') {
-        element.style.backgroundColor = 'green';
-    } else {
-        element.style.backgroundColor = 'red';
+    success(text) {
+        this.createElement();
+        this.element.innerText = text;
+        this.element.style.backgroundColor = 'green';
+        this.show();
     }
 
-    element.style.display = 'block';
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function() {
-        element && (element.style.display = 'none');
-    }, 5000);
-};
-
-module.exports = {
-    success: function(text) {
-        Alert(text, 'success');
-    },
-    error: function(text) {
-        Alert(text, 'error');
+    error(text) {
+        this.createElement();
+        this.element.innerText = text;
+        this.element.style.backgroundColor = 'red';
+        this.show();
     }
-};
+
+    destroy() {
+        if (this.element && this.element.parentNode) {
+            this.element.parentNode.removeChild(this.element);
+        }
+    }
+
+    show() {
+        this.element.style.display = 'block';
+        clearTimeout(this.timeoutId);
+        this.timeoutId = setTimeout(() => {
+            this.element && (this.element.style.display = 'none');
+        }, 5000);
+    }
+}
+
+export default new Alert();
